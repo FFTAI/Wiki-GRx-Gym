db46b30c732bb8b8c16a3b6f3877049d07253ab6
Jason Chen <chenxin199305@163.com>
2024-11-21 23:35:07 +0800

    Update legged_robot_fftai_config.py

--- legged_gym/legged_gym/envs/fftai/legged_robot_fftai_config.py
+++ legged_gym/legged_gym/envs/fftai/legged_robot_fftai_config.py
@@ -3,6 +3,13 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 
 class LeggedRobotFFTAICfg(LeggedRobotCfg):
+    class sim(LeggedRobotCfg.sim):
+        dt = 0.001
+
+        class physx(LeggedRobotCfg.sim.physx):
+            num_position_iterations = 4
+            num_velocity_iterations = 0
+
     class env(LeggedRobotCfg.env):
         num_obs = 1
         num_actions = 1
@@ -10,13 +17,6 @@ class LeggedRobotFFTAICfg(LeggedRobotCfg):
     class rewards(LeggedRobotCfg.rewards):
         sigma_action_diff = -0.1
         sigma_action_diff_diff = -1.0
-
-    class sim(LeggedRobotCfg.sim):
-        dt = 0.001
-
-        class physx(LeggedRobotCfg.sim.physx):
-            num_position_iterations = 4
-            num_velocity_iterations = 0
 
 
 class LeggedRobotFFTAICfgPPO(LeggedRobotCfgPPO):
